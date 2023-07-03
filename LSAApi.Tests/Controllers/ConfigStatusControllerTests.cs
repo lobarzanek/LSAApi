2a6c72ad747dd2a49c455638d16eddc761f3c387
b0r55uk <101877993+b0r55uk@users.noreply.github.com>
2023-07-03 16:43:36 +0200

    Added unit tests for ConfigStatusController and ConfigurationController

---
+++ LSAApi.Tests/Controllers/ConfigStatusControllerTests.cs
@@ -0,0 +1,123 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using LSAApi.Controllers;
+using LSAApi.Dto;
+using LSAApi.Interfaces;
+using LSAApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LSAApi.Tests.Controllers
+{
+    public class ConfigStatusControllerTests
+    {
+        private readonly IConfigStatusRepository _configStatusRepository;
+        private readonly IMapper _mapper;
+        private readonly ConfigStatusController _configStatusController;
+        public ConfigStatusControllerTests()
+        {
+            _configStatusRepository = A.Fake<IConfigStatusRepository>();
+            _mapper = A.Fake<IMapper>();
+            _configStatusController = new ConfigStatusController(_configStatusRepository, _mapper);
+        }
+
+        [Fact]
+        public void ConfigStatusController_GetConfigStatuses_ReturnOK()
+        {
+            //Arrange
+            var configStatuses = A.Fake<ICollection<GetConfigStatusDto>>();
+            var configStatusesList = A.Fake<List<GetConfigStatusDto>>();
+            A.CallTo(() => _mapper.Map<List<GetConfigStatusDto>>(configStatuses))
+                .Returns(configStatusesList);
+
+            //Act
+            var result = _configStatusController.GetConfigStatuses();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+        [Fact]
+        public void ConfigStatusController_GetConfigStatus_ReturnOK()
+        {
+            //Arrange
+            int id = 1;
+            var configStatusDto = A.Fake<GetConfigStatusDto>();
+
+            A.CallTo(() => _configStatusRepository.IsExist(id)).Returns(true);
+            A.CallTo(() => _mapper.Map<GetConfigStatusDto>(_configStatusRepository.GetConfigStatus(id))).Returns(configStatusDto);
+            //Act
+            var result = _configStatusController.GetConfigStatus(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void ConfigStatusController_CreateConfigStatus_ReturnCreated()
+        {
+            //Arrange
+            var createConfigStatus = A.Fake<CreateConfigStatusDto>();
+            var configStatus = A.Fake<ConfigStatus>();
+
+            A.CallTo(() => _mapper.Map<ConfigStatus>(createConfigStatus)).Returns(configStatus);
+            A.CallTo(() => _configStatusRepository.CreateConfigStatus(configStatus)).Returns(true);
+
+            //Act
+            var result = _configStatusController.CreateConfigStatus(createConfigStatus);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<CreatedResult>();
+
+        }
+
+        [Fact]
+        public void ConfigStatusController_UpdateConfigStatus_ReturnOK()
+        {
+            //Arrange
+
+            var updateConfigStatus = A.Fake<UpdateConfigStatusDto>();
+            var configStatus = A.Fake<ConfigStatus>();
+
+            A.CallTo(() => _configStatusRepository.IsExist(updateConfigStatus.ConfigStatusId)).Returns(true);
+            A.CallTo(() => _mapper.Map<ConfigStatus>(updateConfigStatus)).Returns(configStatus);
+            A.CallTo(() => _configStatusRepository.UpdateConfigStatus(configStatus)).Returns(true);
+
+            //Act
+            var result = _configStatusController.UpdateConfigStatus(updateConfigStatus);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+
+        }
+
+        [Fact]
+        public void ConfigStatusController_DeleteConfigStatus_ReturnNoContent()
+        {
+            //Arrange
+            int id = 1;
+            var configStatus = A.Fake<ConfigStatus>();
+
+            A.CallTo(() => _configStatusRepository.IsExist(id)).Returns(true);
+            A.CallTo(() => _configStatusRepository.GetConfigStatus(id)).Returns(configStatus);
+            A.CallTo(() => _configStatusRepository.DeleteConfigStatus(configStatus)).Returns(true);
+
+            //Act
+            var result = _configStatusController.DeleteConfigStatus(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<NoContentResult>();
+
+        }
+    }
+}
---
+++ LSAApi.Tests/Controllers/ConfigurationControllerTests.cs
@@ -0,0 +1,198 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using LSAApi.Controllers;
+using LSAApi.Dto;
+using LSAApi.Interfaces;
+using LSAApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LSAApi.Tests.Controllers
+{
+    public class ConfigurationControllerTests
+    {
+        private readonly IConfigurationRepository _configurationRepository;
+        private readonly ISwitchRepository _switchRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly ISwitchStatusRepository _switchStatusRepository;
+        private readonly IConfigStatusRepository _configStatusRepository;
+        private readonly IMapper _mapper;
+        private readonly ConfigurationController _configurationController;
+
+        public ConfigurationControllerTests()
+        {
+            _configurationRepository = A.Fake<IConfigurationRepository>();
+            _switchRepository = A.Fake<ISwitchRepository>();
+            _userRepository = A.Fake<IUserRepository>();
+            _switchStatusRepository = A.Fake<ISwitchStatusRepository>();
+            _configStatusRepository = A.Fake<IConfigStatusRepository>();
+            _mapper = A.Fake<IMapper>();
+            _configurationController = new ConfigurationController(_configurationRepository, _switchRepository,
+                _userRepository, _switchStatusRepository, _configStatusRepository, _mapper);
+        }
+
+        [Fact]
+        public void ConfigurationController_GetConfigurations_ReturnsOK()
+        {
+            //Arrange
+            var configurations = A.Fake<ICollection<GetConfigurationDto>>();
+            var configurationsList = A.Fake<List<GetConfigurationDto>>();
+
+            A.CallTo(() => _mapper.Map<List<GetConfigurationDto>>(configurations)).Returns(configurationsList);
+
+            //Act
+            var result = _configurationController.GetConfigurations();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+        [Fact]
+        public void ConfigurationController_GetConfiguration_ReturnsOK()
+        {
+            //Arrange
+            int id = 1;
+            var configuration = A.Fake<Configuration>();
+            var configurationDto = A.Fake<GetConfigurationDto>();
+
+            A.CallTo(() => _configurationRepository.IsExist(id)).Returns(true);
+            A.CallTo(() => _mapper.Map<GetConfigurationDto>(_configurationRepository.GetConfigurationById(id)))
+                .Returns(configurationDto);
+
+            //Act
+            var result = _configurationController.GetConfiguration(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+        [Fact]
+        public void ConfigurationController_GetConfigurationsBySwitch_ReturnsOK()
+        {
+            //Arrange
+            int id = 1;
+            var configuration = A.Fake<Configuration>();
+            var configurationsDto = A.Fake<List<GetConfigurationDto>>();
+
+            A.CallTo(() => _switchRepository.IsExist(id)).Returns(true);
+            A.CallTo(() => _mapper.Map<List<GetConfigurationDto>>(_configurationRepository.GetConfigurationsBySwitch(id)))
+                .Returns(configurationsDto);
+
+            //Act
+            var result = _configurationController.GetConfigurationsBySwitch(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void ConfigurationController_GetConfigurationsByUser_ReturnsOK()
+        {
+            //Arrange
+            int id = 1;
+            var configuration = A.Fake<Configuration>();
+            var configurationsDto = A.Fake<List<GetConfigurationDto>>();
+
+            A.CallTo(() => _userRepository.IsExist(id)).Returns(true);
+            A.CallTo(() => _mapper.Map<List<GetConfigurationDto>>(_configurationRepository.GetConfigurationsByUser(id)))
+                .Returns(configurationsDto);
+
+            //Act
+            var result = _configurationController.GetConfigurationsByUser(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void ConfigurationController_GetConfigurationsByStatus_ReturnsOK()
+        {
+            //Arrange
+            int id = 1;
+            var configuration = A.Fake<Configuration>();
+            var configurationsDto = A.Fake<List<GetConfigurationDto>>();
+
+            A.CallTo(() => _switchStatusRepository.IsExist(id)).Returns(true);
+            A.CallTo(() => _mapper.Map<List<GetConfigurationDto>>(_configurationRepository.GetConfigurationsByStatus(id)))
+                .Returns(configurationsDto);
+
+            //Act
+            var result = _configurationController.GetConfigurationsByStatus(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void ConfigurationController_CreateConfiguration_ReturnsCreated()
+        {
+            //Arrange
+            var configuration = A.Fake<Configuration>();
+            var createConfiguration = A.Fake<CreateConfigurationDto>();
+
+            A.CallTo(() => _switchRepository.IsExist(createConfiguration.SwitchId)).Returns(true);
+            A.CallTo(() => _userRepository.IsExist(createConfiguration.UserId)).Returns(true);
+            A.CallTo(() => _mapper.Map<Configuration>(createConfiguration)).Returns(configuration);
+            A.CallTo(() => _configurationRepository.CreateConfiguration(configuration)).Returns(true);
+
+
+            //Act
+            var result = _configurationController.CreateConfiguration(createConfiguration);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<CreatedResult>();
+        }
+
+        [Fact]
+        public void ConfigurationController_UpdateConfiguration_ReturnsOK()
+        {
+            //Arrange
+            var configuration = A.Fake<Configuration>();
+            var updateConfiguration = A.Fake<UpdateConfigurationDto>();
+
+            A.CallTo(() => _configurationRepository.IsExist(updateConfiguration.ConfigurationId)).Returns(true);
+            A.CallTo(() => _switchRepository.IsExist(updateConfiguration.SwitchId)).Returns(true);
+            A.CallTo(() => _userRepository.IsExist(updateConfiguration.UserId)).Returns(true);
+            A.CallTo(() => _configStatusRepository.IsExist(updateConfiguration.ConfigStatusId)).Returns(true);
+            A.CallTo(() => _mapper.Map<Configuration>(updateConfiguration)).Returns(configuration);
+            A.CallTo(() => _configurationRepository.GetConfigurationById(updateConfiguration.ConfigurationId)).Returns(configuration);
+            A.CallTo(() => _configurationRepository.UpdateConfiguration(configuration)).Returns(true);
+
+            //Act
+            var result = _configurationController.UpdateConfiguration(updateConfiguration);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void ConfigurationController_DeleteConfiguration_ReturnsNoContent()
+        {
+            //Arrange
+            int id = 1;
+            var configuration = A.Fake<Configuration>();
+
+            A.CallTo(() => _configurationRepository.IsExist(id)).Returns(true);
+            A.CallTo(() => _configurationRepository.GetConfigurationById(id)).Returns(configuration);
+            A.CallTo(() => _configurationRepository.DeleteConfiguration(configuration)).Returns(true);
+
+            //Act
+            var result = _configurationController.DeleteConfiguration(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<NoContentResult>();
+        }
+    }
+}
